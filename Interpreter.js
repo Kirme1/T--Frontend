f87def1d4282ebc71473eb8437f34726ef226d69
nihiliss <samcenko@student.chalmers.se>
2022-12-22 20:56:15 +0100

    #14 Add fault tolerance measures

--- Interpreter.js
+++ Interpreter.js
@@ -4,6 +4,7 @@ const client = mqtt.connect("mqtt://localhost:1883/")
 const Api = axios.create({
     baseURL: process.env.VUE_APP_API_ENDPOINT || 'http://localhost:8000/api'
 })
+var CommandsFactory = require('hystrixjs').commandFactory;
 
 client.on("connect", e => {
     console.log("connected")
@@ -14,18 +15,21 @@ client.on("connect", e => {
                 try {
                     let message = JSON.parse(m.toString())
                     if (message.request === 'post') {
-                        postRequest(message.url, message.data, message.data.Authorization).then(data => {
+                        var postPromise = postCommand.execute(message.url, message.data, message.data.Authorization) //Execute the command, creating a promise
+                        postPromise.then(data => { //Once the promise is fulfilled, execute the rest of the code
                             let response = { "id": message.id, "response": "response", "data": data }
                             return client.publish(topic, JSON.stringify(response), {qos:1})
                         })
                     } else if (message.request === 'get') {
-                        getRequest(message.url, message.data.Authorization).then(data => {
+                        var getPromise = getCommand.execute(message.url, message.data.Authorization)
+                        getPromise.then(data => {
                             let response = { "id": message.id, "response": "response", "data": data }
                             return client.publish(topic, JSON.stringify(response), {qos:1})
                         })
                     } else if (message.request === 'book') {
                         console.log('here')
-                        book(message.url, message.data).then(data => {
+                        var bookPromise = bookCommand.execute(message.url, message.data) //Execute the command, creating a promise
+                        bookPromise.then(data => { //Once the promise is fulfilled, execute the rest of the code
                             let response = { "id": message.id, "response": "response", "data": data }
                             return client.publish(topic, JSON.stringify(response), {qos:1})
                         })
@@ -41,6 +45,17 @@ client.on("connect", e => {
     })
 })
 
+var bookCommand = CommandsFactory.getOrCreate("Service on port :" + service.port + ":" + port) //Create a book command with default settings
+    .circuitBreakerErrorThresholdPercentage(service.errorThreshold)
+    .timeout(service.timeout)
+    .run(book) //The command calls the book function when executed, creating a promise
+    .circuitBreakerRequestVolumeThreshold(service.concurrency)
+    .circuitBreakerSleepWindowInMilliseconds(service.timeout)
+    .statisticalWindowLength(10000)
+    .statisticalWindowNumberOfBuckets(10)
+    .errorHandler(isErrorHandler)
+    .build();
+
 async function book(url, data) {
     console.log(data)
     let res = {}
@@ -53,6 +68,18 @@ async function book(url, data) {
     console.log(res)
     return res
 }
+
+var getCommand = CommandsFactory.getOrCreate("Service on port :" + service.port + ":" + port) //Same as above, but for the getRequest function
+    .circuitBreakerErrorThresholdPercentage(service.errorThreshold)
+    .timeout(service.timeout)
+    .run(getRequest)
+    .circuitBreakerRequestVolumeThreshold(service.concurrency)
+    .circuitBreakerSleepWindowInMilliseconds(service.timeout)
+    .statisticalWindowLength(10000)
+    .statisticalWindowNumberOfBuckets(10)
+    .errorHandler(isErrorHandler)
+    .build();
+
 async function getRequest(url, Autho) {
     let data = {}
     if(Autho != undefined){
@@ -71,6 +98,17 @@ async function getRequest(url, Autho) {
         return data
     }
 }
+
+var postCommand = CommandsFactory.getOrCreate("Service on port :" + service.port + ":" + port) //Same as above, but for the postRequest function
+    .circuitBreakerErrorThresholdPercentage(service.errorThreshold)
+    .timeout(service.timeout)
+    .run(postRequest)
+    .circuitBreakerRequestVolumeThreshold(service.concurrency)
+    .circuitBreakerSleepWindowInMilliseconds(service.timeout)
+    .statisticalWindowLength(10000)
+    .statisticalWindowNumberOfBuckets(10)
+    .errorHandler(isErrorHandler)
+    .build();
 
 async function postRequest(url, data, Autho) {
     let res = {}
